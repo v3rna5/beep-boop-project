74d34b3656c8e248d7931fd99a13c3ae67181296
verna <vernajs@gmail.com>
2018-03-30 20:23:19 -0700

    Fix codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,33 @@
-//Business Logic
-var Words = function(num) {
-var arrnumbers = [1,2,3,4,5,6,7,8,9,0]
-var num;
-for (var i=0; i<arrnumbers.length; i++) {
-for (var j = 0; j<num.lenght; i++) {
-
-}
-}
-
-  if ((num === 0)) {
-    return $(".True").text("Beep!");;
-  }
-  if (( num === 1)) {
-    $(".True").text("Boop!");
-}
-  if (( num === 3)) {
-    $(".True").text("I'm sorry, Dave. I'm afraid I can't do that.");
-}
-}
-
-
-// user interface logic
 $(document).ready(function() {
-  $("form#NumberToWord").submit(function(event) {
+  $("form#numwords").submit(function(event) {
     event.preventDefault();
-    var num = parseInt($("input#numbers").val());
-    var result = Words(num);
+    var input = $("input#number").val();
+    var results = "";
 
-    $("#resultTrue").show();
+    for(var i=0; i<input.length; i++ ) {
+	     if( input[i] === '0' ) {
+		       results = "Beep";
+        } else {
+		        results += input[i];
+        }
+
+           if( input[i] === '1' ) {
+               results = "Boop";
+            } else {
+                results += input[i];
+            }
+
+               if( input[i] === '3' ) {
+                   results = "David";
+                } else {
+                    results += input[i];
+                }
+}
+
+
+
+
+    $("#output").text(results);
+    $("#output").show();
   });
 });
